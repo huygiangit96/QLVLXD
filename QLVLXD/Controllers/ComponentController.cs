d6a731aaf92e638c1b2ea6dd48d0715c9f6c4eaa
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2018-05-21 14:18:05 +0700

    Second

---
+++ QLVLXD/Controllers/ComponentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLVLXD.Controllers
+{
+    public class ComponentController : SecurityController
+    {
+        // GET: Component
+        public ActionResult Employee()
+        {
+            return View("Employee");
+        }
+        public ActionResult Supplier()
+        {
+            return View("Supplier");
+        }
+        public ActionResult ConstructorTeam()
+        {
+            return View("Constructor");
+        }
+    }
+}
---
+++ QLVLXD/Controllers/StorageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLVLXD.Controllers
+{
+    public class StorageController : Controller
+    {
+        // GET: Storage
+        public ActionResult StockIn()
+        {
+            return View("Stockin");
+        }
+        public ActionResult StockOut()
+        {
+            return View("Stockout");
+        }
+    }
+}
